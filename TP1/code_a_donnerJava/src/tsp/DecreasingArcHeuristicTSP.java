982efc7a7988a707179b17a4850cba52b4210192
DURY Victor <vdury@opium.pedago.ipb.fr>
2015-02-04 17:28:18 +0100

    decreasing Arcs

--- TP1/code_a_donnerJava/src/tsp/DecreasingArcHeuristicTSP.java
+++ TP1/code_a_donnerJava/src/tsp/DecreasingArcHeuristicTSP.java
@@ -2,14 +2,15 @@ package tsp;
 import java.util.List;
 import java.util.ArrayList;
 import java.awt.Point;
-
+import java.util.Collections;
+import tsp.Arc;
 /**
  * This heuristic sorts the arcs by increasing value and 
  * considers each arc in turn for insertion
  * An arc is inserted if and only if it does not create a subtour.
  * The method stops when a tour is obtained.
  */
-public class DecreasingArcHeuristicTSP implements HeuristicTSP {
+public class DecreasingArcHeuristicTSP implements HeuristicTSP{
 
 	/** TODO coder cette méthode */
 	public double computeSolution(double[][] matrix, List<Integer> solution) {
@@ -17,17 +18,29 @@ public class DecreasingArcHeuristicTSP implements HeuristicTSP {
 		int i,j;
 		int n = matrix[1].length;
 		int m = n*(n - 1);
+		int min = 100000;
 		//List<Point> tri_arcs = new List<>(); 
 		//		for (i=0; i<
 		//Point[] tab = new Point[m];
-		List<> list = new ArrayList<>();
+		ArrayList<Arc> list = new ArrayList<Arc>();
 		for (i=0; i<n; i++) {
 		    for (j= 0; j< n; j++) {
 			if (i != j) {
-			    list.append(i, j, matrix[i][j]);
-			    //			    Collections.sort
-
+			    list.add(new Arc(matrix[i][j], i, j));
+			    Collections.sort(list);
 			}
+		    }
+		}
+
+		//list.sort();
+		//ArrayList<Integer> list2 = newArrayList<Integer>();
+		solution.add(list.get(0).getStart());
+		solution.add(list.get(0).getFinish());
+		value = list.get(0).getValue();
+		for(Arc arc: list){
+		    if(!solution.contains(arc.getFinish())) {
+			solution.add(arc.getFinish());
+			value += arc.getValue();
 		    }
 		}
 		return value;
